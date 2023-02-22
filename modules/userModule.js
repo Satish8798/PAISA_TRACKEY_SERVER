a611606eae86ab72b04c0647bb602457c0ecebd6
Satish Ganesan <ganesansatishm7@gmail.com>
2023-02-22 15:12:33 +0530

    added profile update

--- modules/userModule.js
+++ modules/userModule.js
@@ -1,55 +1,71 @@
 const userModel = require("../models/userModel");
 const bcrypt = require("bcrypt");
 
-module.exports.signup = async (req,res) => {
-    const {name,email,password} = req.body;
-    try {
-        const emailExists = await userModel.findOne({email: email});
+module.exports.signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const emailExists = await userModel.findOne({ email: email });
 
-        if(emailExists){
-            throw new Error("Email aready exists");
-        }
-
-        const randomStraing = await bcrypt.genSalt(10);
-
-        const hashedPassword = await bcrypt.hash(password,randomStraing);
-
-        const user = new userModel({
-            name,email,password:hashedPassword
-        })
-        
-        const savedUser = await user.save();
-
-        if(savedUser){
-           return  res.status(200).send(true);
-        }
-    } catch (error) {
-        return res.status(400).send(error.message);
+    if (emailExists) {
+      throw new Error("Email aready exists");
     }
-}
 
-module.exports.login = async (req,res)=> {
-    const {email,password} = req.body;
-    try {
-        const user = await userModel.findOne({email});
+    const randomStraing = await bcrypt.genSalt(10);
 
-        if(!user){
-            throw new Error("Account does not exist");
-        }
-        const passwordMatching = await bcrypt.compare(password,user.password);
+    const hashedPassword = await bcrypt.hash(password, randomStraing);
 
-        if(!passwordMatching){
-            throw new Error("password incorrect");
-        }
+    const user = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
-        res.status(200).send({
-            name: user.name,
-            email: user.email,
-            _id: user._id,
-            balance: user.balance
-        })
+    const savedUser = await user.save();
 
-    } catch (error) {
-        return res.status(400).send(error.message);
+    if (savedUser) {
+      return res.status(200).send(true);
     }
-}
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+};
+
+module.exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      throw new Error("Account does not exist");
+    }
+    const passwordMatching = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatching) {
+      throw new Error("password incorrect");
+    }
+
+    res.status(200).send({
+      name: user.name,
+      email: user.email,
+      _id: user._id,
+      balance: user.balance,
+    });
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+};
+
+module.exports.update = async (req, res) => {
+  try {
+    const { name, email, id } = req.body;
+    const user = await userModel.findByIdAndUpdate(
+      { _id: id },
+      { email:email, name:name },
+      { returnDocument: "after" }
+    );
+
+    res.status(200).send(user)
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,8 +1,9 @@
-const { signup, login } = require("../modules/userModule");
+const { signup, login, update } = require("../modules/userModule");
 
 const router = require("express").Router();
 
 router.post('/login',login);
 router.post('/signup',signup);
+router.put('/update',update);
 
 module.exports = router;
